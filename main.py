fbebda06651899b7ac8882db8afbf06a56d8f960
aleynak-git <kofaleyna@gmail.com>
2021-04-03 13:04:30 +0300

    main.py is updated and send function is embedded to main.py

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import random
+import csv
+import datetime
+import send
 
 # User-agent tanımlıyorum, scraping yaparken beni browser sansın
-user_agent_list = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36',
-                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
-                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
-                   'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1']
+user_agent_list = [
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/87.0.4280.77 Mobile/15E148 Safari/604.1']
 
-# agent_header = {
-#    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+# agent_header = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
+# Chrome/89.0.4389.82 Safari/537.36'}
 
 sembol_liste = ["AMZN", "GOOG", "FB", "NFLX", "MSFT"]
+today = str(datetime.date.today()) + ".csv"
+csv_file = open(today, "w", newline='')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Stock Name', 'Current Name', 'Previous Close',
+                     'Open', 'Bid', 'Ask', "Day's Range",
+                     '52 Week Range', 'Volume', 'Avg. Volume'])
 
 # sembol = input("Hisse sembolünü giriniz")
 # hisse_url = "https://finance.yahoo.com/quote/NFLX?p=NFLX&.tsrc=fin-srch"
 # print(hisse_url)
 
 for sembol in sembol_liste:
+
+    stock = []
+
     user_agent = random.choice(user_agent_list)
     agent_header = {'User-Agent': user_agent}
     hisse_url = "https://finance.yahoo.com/quote/" + sembol + "?p=" + sembol + "&.tsrc=fin-srch"
@@ -46,6 +59,9 @@ for sembol in sembol_liste:
     print(hisse_fiyat)
     print(hisse_title + " - " + hisse_fiyat)
 
+    stock.append(str(hisse_title))
+    stock.append(float(hisse_fiyat.replace(',', '')))
+
     table_info = soup.find('div', class_="D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) "
                                          "smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY "
                                          "smartphone_Bdc($seperatorColor)")
@@ -60,9 +76,12 @@ for sembol in sembol_liste:
 
         print(satir_baslik)
         print(satir_deger)
-
+        stock.append(satir_deger.replace(',', ''))
+    csv_writer.writerow(stock)
     print("----------------------")
     # sitenin engellememesi için 5 sn uyut
     time.sleep(5)
-
+csv_file.close()
+# mail göndermek için önce dosyayı kapatmak gerekli
+send.send("Homework_2.pdf")
 print("end-of-list")
---
+++ send.py
@@ -0,0 +1,60 @@
+import datetime
+import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+# HOCANIN KODU
+
+
+def send(filename):
+    # 587=port
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('ptn8403@gmail.com', 'BA_Python_2021')
+
+    from_address = 'ptn8403@gmail.com'
+    to_address = 'kofaleyna@gmail.com'
+
+    # to_list = ['borayuret@bilgeadam.com',
+    #            # "utku.gokcen13@gmail.com",
+    #            "kofaleyna@gmail.com"]
+
+    subject = "YFscrapper'dan mail geldi! " + datetime.datetime.now().strftime('%H:%M:%S')
+
+    msg = MIMEMultipart()
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Subject'] = subject
+
+    # today = str(datetime.date.today()) + ".csv"
+    my_attachment = open(filename, 'rb')
+
+    part = MIMEBase('application', 'octet-stream')
+    # pdf dosyası göndermek istersek:
+    # part = MIMEBase('application', 'pdf')
+    part.set_payload(my_attachment.read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment;filename=' + filename)
+    msg.attach(part)
+
+    body = """<b>Selamlar<b> <hr/> 
+    Python Programından mail gönderiyoruz. <br/> 
+    Aleyna <hr/>
+    <a href=https://www.koredenkozmetik.com.tr/collections/tum-urunler>Visit Page</a>
+    """
+    msg.attach(MIMEText(body, 'html'))
+    message = msg.as_string()
+
+    # for to_single in to_list:
+    server.sendmail('ptn8403@gmail.com', to_address, message)
+
+    # msg = MIMEText(message, "plain", "UTF-8")
+    # server.sendmail(from_address, to_address, msg.as_string())
+
+    # for to_single in to_list:
+    #    server.sendmail(from_address, to_list, msg.as_string())
+
+    server.quit()
